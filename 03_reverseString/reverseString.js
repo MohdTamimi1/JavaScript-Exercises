eef42c4268fcbfd865423c66cca7152bac932881
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-03-22 20:55:09 -0400

    Windows Test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,12 @@
 const reverseString = function (str) {
-    let reverseStr = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        reverseStr += str[i];
-    }
-    return reverseStr;
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
 };
+
+// Windows Test
 
 // Do not edit below this line
 module.exports = reverseString;
