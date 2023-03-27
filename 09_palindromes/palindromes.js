2c2313cb2b2aa77d36c0805364d59cd06d24be0e
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-03-27 11:49:47 -0400

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
-
+// console.log(palindromes("racecar"));
+// console.log(palindromes("hello"));
 // Do not edit below this line
 module.exports = palindromes;
