1b10f7592fbcab29430b5b037325677cfd0cb538
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-03-28 19:44:04 -0400

    Solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = (list) => {
+    let newList = [];
+    for (let i = 0; i < list.length; i++) {
+        newList.push(list[i].title);
+    }
+    return newList;
 };
 
 // Do not edit below this line
