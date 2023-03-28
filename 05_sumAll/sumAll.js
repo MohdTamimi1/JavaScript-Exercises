fd4a6511a60ce714fa79b89cd7c541965805827f
MohdTamimi1 <mohd.tt1987@gmail.com>
2023-03-28 18:16:04 -0400

    Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,9 @@
 const sumAll = function (a, b) {
-  let sum = 0;
-  for (let i = a; i <= b; i++) {
-    sum += i;
-  }
-  return sum;
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
