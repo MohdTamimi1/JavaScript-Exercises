d7a51b6a758d8941650b69ec0875210861c31de5
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-03-28 20:58:26 -0400

    Solved

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,20 @@
-const caesar = function() {
+const caesar = (str, shift) => {
+    let newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i);
 
+        if (charCode >= 65 && charCode <= 90) {
+            newStr += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+        } else if (charCode >= 97 && charCode <= 122) {
+            newStr += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+        } else {
+            newStr += str[i];
+        }
+    }
+    return newStr;
 };
+
+// console.log(caesar("Hello, World!", 3)); // Khoor, Zruog!
 
 // Do not edit below this line
 module.exports = caesar;
