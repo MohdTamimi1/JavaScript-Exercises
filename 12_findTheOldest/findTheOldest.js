0478014c7568d11aa12b1d762843e66edcfc7451
Mohd Tamimi <mohd.tt1987@gmail.com>
2023-03-28 20:29:53 -0400

    Solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = (list) => {
+    let oldest = [];
+    let temp = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].yearOfDeath) {
+            temp = list[i].yearOfDeath - list[i].yearOfBirth;
+        }
+        oldest.push(temp);
+    }
+    return list[oldest.indexOf(Math.max(...oldest))];
 };
+
+// const list = [
+//     {
+//         name: "Carly",
+//         yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//     },
+//     {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//     },
+//     {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//     },
+// ];
+
+// console.log(findTheOldest(list));
 
 // Do not edit below this line
 module.exports = findTheOldest;
